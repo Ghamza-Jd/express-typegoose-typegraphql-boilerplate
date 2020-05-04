0caa5a2baefdec65bf544a4383a69dbec2953f40
HamzaJadid <hamzah.jadeed@gmail.com>
2020-05-04 07:00:36 +0300

    Added logger for graphql requests

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { authChecker } from './auth-checker';
+import morgan from 'morgan';
 
 import { LoginResolver, UserResolver } from './modules/user/resolver';
 
@@ -15,8 +16,22 @@ export default async function server() {
     authMode: 'null',
   });
 
+  morgan.token('graphql-query', (req) => {
+    const { query } = req.body;
+    return `GRAPHQL:\n${query}\b`;
+  });
+
+  app.use(
+    morgan(':graphql-query', {
+      skip(req: express.Request): boolean {
+        const { query } = req.body;
+        return query?.toString().includes('IntrospectionQuery');
+      },
+    }),
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
   app.use(cors());
 
   const apolloServer = new ApolloServer({
